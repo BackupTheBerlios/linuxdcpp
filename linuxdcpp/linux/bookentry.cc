727595b9c49a219d80660c92b2afc228cc64c228
paskharen <paskharen>
2004-12-30 20:47:42 +0000

    Fixed the close button on the folder flaps

--- linuxdcpp/linux/bookentry.cc
+++ linuxdcpp/linux/bookentry.cc
@@ -6,11 +6,18 @@ BookEntry::BookEntry(int type, string id, string title, GCallback closeCallback)
 	this->type = type;
 	this->id = id;
 	
-	label = GTK_LABEL(gtk_label_new(title.c_str()));
-	button = GTK_BUTTON(gtk_button_new_from_stock(GTK_STOCK_CLOSE));
 	box = gtk_hbox_new(FALSE, 5);
+
+	label = GTK_LABEL(gtk_label_new(title.c_str()));
 	gtk_box_pack_start(GTK_BOX(box), GTK_WIDGET(label), FALSE, TRUE, 0);
+
+	button = GTK_BUTTON(gtk_button_new());
+	gtk_container_add(GTK_CONTAINER(button), 
+		gtk_image_new_from_stock(GTK_STOCK_CLOSE, GTK_ICON_SIZE_MENU));
+	gtk_widget_set_size_request(GTK_WIDGET(button), 16, 16);
+	gtk_button_set_relief(button, GTK_RELIEF_NONE);
 	gtk_box_pack_start(GTK_BOX(box), GTK_WIDGET(button), FALSE, TRUE, 0);
+
 	gtk_widget_show_all(box);
 	
     g_signal_connect(G_OBJECT(button), "clicked", 
